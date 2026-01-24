f90ff3ebcd356c04cf5cde66c56f3cb3bd3b78f5
Ulaş Sertan KEMEÇ <sertan.usk@gmail.com>
2026-01-25 01:23:04 +0300

    Prototyped the functions for GEMMKernel

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include "Matrix.hpp"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    const Matrix<float> A(1, 1);
+
+    const Matrix<float> B(1, 1);
+
+    Matrix <float> C = A + B;
     return 0;
 }
